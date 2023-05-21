8eb12319a323cf29bd48be6f47ae071227eb60c8
Shubham Majumdar <shubham.majumdar@ssipmt.com>
2023-05-21 19:41:26 +0530

    edited

--- array.js
+++ array.js
@@ -1,3 +1,22 @@
-let fruits = ["apple", "banana", "orange", "grape", "mango"];
-console.log(fruits.length);
+var fruits = ["apple", "banana", "orange", "grape", "mango", 100402, true];
+// console.log(fruits.length);
 
+var fruit1 =[1,2,3,4,5,6,7,fruits];
+console.log(fruit1);
+console.log(fruit1.length);
+// console.log(typeof(fruits));
+
+//insert another element in js
+fruits.push("Papaya");
+fruits.push("Litchi");
+console.log(fruits);
+
+//pop element
+fruits.pop();
+console.log(fruits);
+
+//concatination
+var fruit2= ["aam","seb","chiku"];
+
+var fruit_total=fruit1.concat(fruit2);
+console.log(fruit_total);
--- sum.js
+++ sum.js
@@ -1,13 +1,19 @@
-var a;
+const a='shubh';
+//  var a;
 var b;
 
-a=10;
+// a=10;
 b=15;
 
 c=a+b;
 
-// console.log(a);
-// console.log(b);
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(a);
+console.log(b);
 console.log(c);
+console.log(a+b);
 
 // console.log(b,a);
