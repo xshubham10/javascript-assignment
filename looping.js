5bb5906f198c3165d4e62ec3dccd5a544c8bd6ba
Shubham Majumdar <shubham.majumdar@ssipmt.com>
2023-05-24 11:09:22 +0530

    1

--- looping.js
+++ looping.js
@@ -1,1 +1,38 @@
-// how to link your js in your html.
+// // how to link your js in your html.
+// console.log('hello everyone! This is the output for day2')
+
+// const obj = {
+//     key1: 'value1',
+//     key2: 'value2',
+    
+// }
+
+// console.log(obj.key2);
+// console.log("this is to find the object");
+// console.log(obj['key2']);
+
+// //how to declare objects:
+// var type={
+//     type1: 10,
+//     type2: 20,
+
+// }
+
+// console.log(type.type1);
+// //to print only keys
+// console.log(Object.keys(type));
+
+var a=69;
+var b=100;
+
+if(a>b){
+    console.log("69 > 100");
+}
+
+else if(a<b){
+    console.log("69 < 100");
+}
+
+else{
+    console.log("both are equal");
+}
