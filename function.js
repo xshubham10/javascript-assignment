ea2750cfafc69e0648e9b4ae1440199a9d15ecb1
Shubham Majumdar <shubham.majumdar@ssipmt.com>
2023-05-26 14:48:57 +0530

    1

---
+++ function.js
@@ -0,0 +1,5 @@
+function mult(a,b){ //parameter
+    document.write(a*b);
+}
+document.write("The multiplication of a and b: ");
+mult(20,30);//argument
---
+++ operators.js
@@ -0,0 +1,17 @@
+// var a=10;
+// var b=50;
+// var c=b++;
+
+// console.log(b,c);
+
+
+var a=10;
+var b= '10';
+
+if(a===b){//it compares both number and datatype.
+    console.log("equal");
+}
+
+else{//it compares only numbers.
+    console.log("not equal");
+}
